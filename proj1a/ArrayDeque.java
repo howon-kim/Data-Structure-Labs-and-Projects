e3c1361551d0a26f924e7d3b691b2fd78b0e4c63
Ho Won Kim <howonkim@berkeley.edu>
2019-02-07 10:40:39 -0800

    update ArrayDeque

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -4,7 +4,7 @@ public class ArrayDeque<T> {
     private int nextFirst;
     private int nextLast;
 
-    public ArrayDeque(){
+    public ArrayDeque() {
         items = (T[]) new Object[8];
         size = 0;
         nextFirst = 3;
@@ -16,77 +16,73 @@ public class ArrayDeque<T> {
         nextFirst = other.nextFirst;
         nextLast = other.nextLast;
         items = (T[]) new Object[other.items.length];
-        for(int i = 0; i < items.length; i++){
+        for(int i = 0; i < items.length; i++) {
             items[i] = (T) other.items[i];
         }
     }
 
     /* Adds an item of type T to the front of the deque */
-    public void addFirst(T item){
-        if(items[nextFirst] != null){
+    public void addFirst(T item) {
+        if (items[nextFirst] != null) {
             expandArray();
         }
         items[nextFirst] = item;
         size++;
         nextFirst--;
-        if(nextFirst < 0){
+        if (nextFirst < 0) {
             nextFirst = items.length - 1;
         }
     }
-    public void addLast(T item){
-        if(items[nextLast] != null){
+    public void addLast(T item) {
+        if (items[nextLast] != null) {
             expandArray();
         }
         items[nextLast] = item;
         size++;
         nextLast++;
-        if(nextLast == items.length){
+        if (nextLast == items.length) {
             nextLast = 0;
         }
     }
 
-    public T removeFirst(){
-        if(size <= 0){
+    public T removeFirst() {
+        if (size <= 0) {
             return null;
-        }
-        else{
+        } else {
             T temp;
-            if(nextFirst + 1 == items.length){
+            if (nextFirst + 1 == items.length) {
                 nextFirst = 0;
                 temp = items[nextFirst];
                 items[nextFirst] = null;
-            }
-            else {
+            } else {
                 nextFirst++;
                 temp = items[nextFirst];
                 items[nextFirst] = null;
             }
             size--;
-            if(size / items.length < 0.25 && items.length >= 16){
+            if (size / items.length < 0.25 && items.length >= 16) {
                 shrinkArray();
             }
             return temp;
         }
     }
 
-    public T removeLast(){
-        if(size <= 0){
+    public T removeLast() {
+        if (size <= 0) {
             return null;
-        }
-        else{
+        } else {
             T temp;
-            if(nextLast - 1 < 0){
+            if (nextLast - 1 < 0) {
                 nextLast = items.length - 1;
                 temp = items[nextLast];
                 items[nextLast] = null;
-            }
-            else{
+            } else {
                 nextLast--;
                 temp = items[nextLast];
                 items[nextLast] = null;
             }
             size--;
-            if(size / items.length < 0.25 && items.length >= 16){
+            if (size / items.length < 0.25 && items.length >= 16) {
                 shrinkArray();
             }
             return temp;
@@ -100,23 +96,22 @@ public class ArrayDeque<T> {
         int newIndex = (int) size / 2;
         nextFirst = newIndex - 1;
 
-        for(int i = 0 ; i < size ; i++){
+        for (int i = 0; i < size; i++) {
             newArray[newIndex] = temp[i];
             newIndex++;
         }
-
         nextLast = newIndex;
         items = newArray;
     }
 
-    private void shrinkArray(){
+    private void shrinkArray() {
         int newSize = (int) size / 2;
         T[] newArray = (T[]) new Object[newSize];
         T[] temp = iterateArray();
         int newIndex = (int) newSize / 2;
-        nextFirst = nextFirst - 1;
+        nextFirst = newIndex - 1;
 
-        for(int i = 0; i < size; i++){
+        for(int i = 0; i < size; i++) {
             newArray[newIndex] = temp[i];
             newIndex++;
         }
@@ -124,30 +119,28 @@ public class ArrayDeque<T> {
         items = newArray;
     }
 
-    private T[] iterateArray(){
+    private T[] iterateArray() {
         int first = nextFirst + 1;
         int size = this.size;
         int index = 0;
         T[] newArray = (T[]) new Object[size];
 
-        while(size > 0){
-            if(first >= items.length){
+        while (size > 0) {
+            if (first >= items.length) {
                 first = 0;
-            }
-            else if(items[first] != null) {
+            } else if (items[first] != null) {
                 newArray[index] = items[first];
                 size--;
                 first++;
                 index++;
-            }
-            else {
+            } else {
                 first++;
             }
         }
         return newArray;
     }
 
-    public void printDeque(){
+    public void printDeque() {
         T[] temp = iterateArray();
         for (int i = 0; i < size; i++){
             System.out.print(temp[i].toString() + " ");
@@ -155,21 +148,19 @@ public class ArrayDeque<T> {
         System.out.println();
     }
 
-    public int size(){
+    public int size() {
         return size;
     }
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return size == 0;
     }
 
-    public T get(int index){
-        if(index > size){
+    public T get(int index) {
+        if (index > size) {
             return null;
-        }
-        else if(index + (nextFirst + 1) < items.length){
+        } else if (index + (nextFirst + 1) < items.length) {
             return items[index + (nextFirst + 1)];
-        }
-        else{
+        } else {
             return items[index - (items.length - (nextFirst + 1))];
         }
         /*
@@ -220,6 +211,4 @@ public class ArrayDeque<T> {
 
     }
 */
-
-
 }
