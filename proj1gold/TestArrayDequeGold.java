7d728a7d67d5c0dfff5102d042e562c7e2b0cd75
Ho Won Kim <howonkim@berkeley.edu>
2019-02-14 12:36:50 -0800

    updated the test

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -4,12 +4,86 @@ import org.junit.Test;
 public class TestArrayDequeGold {
 
     @Test
-    public void testAddFirst() {
+    public void testAddFirstRemoveFirst() {
         StudentArrayDeque<Integer> studentDeque = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> solutionDeque = new ArrayDequeSolution<>();
+        for (int testNum = 0; testNum <= 10; testNum++) {
+            String message = "";
+            for (int i = 0; i <= testNum; i++) {
+                Integer number = StdRandom.uniform(10);
+                studentDeque.addFirst(number);
+                solutionDeque.addFirst(number);
+                message += "\naddFirst(" + number + ")";
+            }
+            for (int i = 0; i <= testNum; i++) {
+                Integer actual = studentDeque.removeFirst();
+                Integer expected = solutionDeque.removeFirst();
+                message += "\nremoveFirst(): " + actual;
+                assertEquals(expected, actual);
+            }
+        }
+    }
 
-        studentDeque.addFirst(1);
-        solutionDeque.addFirst(1);
-        assertEquals(studentDeque.removeFirst(), solutionDeque.removeFirst());
+    @Test
+    public void testAddFirstRemoveLast() {
+        StudentArrayDeque<Integer> studentDeque = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> solutionDeque = new ArrayDequeSolution<>();
+        for (int testNum = 0; testNum <= 10; testNum++) {
+            String message = "";
+            for (int i = 0; i <= testNum; i++) {
+                Integer number = StdRandom.uniform(10);
+                studentDeque.addFirst(number);
+                solutionDeque.addFirst(number);
+                message += "\naddFirst(" + number + ")";
+            }
+            for (int i = 0; i <= testNum; i++) {
+                Integer actual = studentDeque.removeLast();
+                Integer expected = solutionDeque.removeLast();
+                message += "\nremoveLast(): " + actual;
+                assertEquals(message, expected, actual);
+            }
+        }
+    }
+
+    @Test
+    public void testAddLastRemoveLast() {
+        StudentArrayDeque<Integer> studentDeque = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> solutionDeque = new ArrayDequeSolution<>();
+        for (int testNum = 0; testNum <= 10; testNum++) {
+            String message = "";
+            for (int i = 0; i <= testNum; i++) {
+                Integer number = StdRandom.uniform(10);
+                studentDeque.addLast(number);
+                solutionDeque.addLast(number);
+                message += "\naddLast(" + number + ")";
+            }
+            for (int i = 0; i <= testNum; i++) {
+                Integer actual = studentDeque.removeLast();
+                Integer expected = solutionDeque.removeLast();
+                message += "\nremoveLast(): " + actual;
+                assertEquals(message, expected, actual);            }
+        }
+    }
+
+
+    @Test
+    public void testAddLastRemoveFirst() {
+        StudentArrayDeque<Integer> studentDeque = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> solutionDeque = new ArrayDequeSolution<>();
+        for (int testNum = 0; testNum <= 10; testNum++) {
+            String message = "";
+            for (int i = 0; i <= testNum; i++) {
+                Integer number = StdRandom.uniform(10);
+                studentDeque.addLast(number);
+                solutionDeque.addLast(number);
+                message += "\naddLast(" + number + ")";
+            }
+            for (int i = 0; i <= testNum; i++) {
+                Integer actual = studentDeque.removeFirst();
+                Integer expected = solutionDeque.removeFirst();
+                message += "\nremoveFirst(): " + actual;
+                assertEquals(expected, actual);
+            }
+        }
     }
 }
