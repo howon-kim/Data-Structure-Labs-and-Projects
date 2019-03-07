ded250269699f4d78d4c59fc643776734d5a3aaf
Ho Won Kim <howonkim@berkeley.edu>
2019-03-06 16:26:10 -0800

    edit hw2

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -4,15 +4,27 @@ import edu.princeton.cs.introcs.StdRandom;
 import edu.princeton.cs.introcs.StdStats;
 
 public class PercolationStats {
-    private PercolationFactory percolation;
+    private Percolation percolation;
     private double [] fractionsOpen;
 
     public PercolationStats(int N, int T, PercolationFactory pf) {
         if (N <= 0 && T <= 0) {
             throw new java.lang.IllegalArgumentException();
         }
-        percolation = pf;
+        percolation = pf.make(N);
         fractionsOpen = new double[T];
+
+        for (int i = 0; i < T ; i++) {
+            while (percolation.percolates()) {
+                int row = StdRandom.uniform(0, N * N);
+                int col = StdRandom.uniform(0, N * N);
+                if (!percolation.isOpen(row, col)) {
+                    percolation.open(row, col);
+                }
+            }
+            fractionsOpen[i] = percolation.numberOfOpenSites() / N * N;
+
+        }
 
     }   // perform T independent experiments on an N-by-N grid
     public double mean() {
