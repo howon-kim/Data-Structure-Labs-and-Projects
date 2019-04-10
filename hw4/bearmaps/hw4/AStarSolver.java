ab21e5fea8e27a00d682fc7b4d4eb84f5d4e712d
Ho Won Kim <howonkim@berkeley.edu>
2019-04-10 00:37:46 -0700

    update

--- hw4/bearmaps/hw4/AStarSolver.java
+++ hw4/bearmaps/hw4/AStarSolver.java
@@ -3,48 +3,91 @@ package bearmaps.hw4;
 import bearmaps.proj2ab.ArrayHeapMinPQ;
 import edu.princeton.cs.algs4.Stopwatch;
 
-import java.util.List;
+import java.util.*;
 
 public class AStarSolver<Vertex> implements ShortestPathsSolver<Vertex> {
-    private ArrayHeapMinPQ fringe = new ArrayHeapMinPQ();
-    private double exploreTime;
-    private SolverOutcome outcome;
-    private List<Vertex> solution;
-    private double solutionWeight;
-    private int statesExplored;
+    private ArrayHeapMinPQ<Vertex> fringe = new ArrayHeapMinPQ();
+    private List<Vertex> solution = new ArrayList<>();
+    private Map<Vertex, Double> distTo = new HashMap<>();
+    private Map<Vertex, Vertex> edgeTo = new HashMap<>();
+    private double exploreTime = 0.0;
+    private double solutionWeight = 0.0;
+    private int statesExplored = 0;
+    private SolverOutcome outcome = SolverOutcome.UNSOLVABLE;
 
 
 
-    public AStarSolver(AStarGraph<Vertex> input, Vertex start, Vertex end, double timeout){
+
+    public AStarSolver(AStarGraph<Vertex> input, Vertex start, Vertex end, double timeout) {
         Stopwatch sw = new Stopwatch();
-        if (explorationTime() > timeout) {
-            outcome = SolverOutcome.UNSOLVABLE;
-        }
+        double heuristic = input.estimatedDistanceToGoal(start, end);
+        fringe.add(start, heuristic);
+        distTo.put(start, 0.0);
 
-        fringe.add(start, 0);
-
-        while (fringe != null) {
+        while (fringe.size() != 0) {
             Vertex v = (Vertex) fringe.removeSmallest();
-            solution.add(v);
+            statesExplored ++;
+            exploreTime = sw.elapsedTime();
 
-            for (WeightedEdge e : input.neighbors(v)){
+            if (v.equals(end)) {
+                outcome = SolverOutcome.SOLVED;
+                Vertex curr = end;
+                while(curr != null){
+                    solution.add(curr);
+                    solutionWeight += distTo.get(curr);
+                    curr = edgeTo.get(curr);
+                }
+                Collections.reverse(solution);
+                return;
+            } else if (sw.elapsedTime() > timeout) {
+                outcome = SolverOutcome.TIMEOUT;
+                return;
+            }
+
+            for (WeightedEdge e : input.neighbors(v)) {
                 Vertex p = (Vertex) e.from();
                 Vertex q = (Vertex) e.to();
                 double w = e.weight();
+                heuristic = input.estimatedDistanceToGoal(q, end);
+                distTo.putIfAbsent(q, Double.POSITIVE_INFINITY);
 
-                fringe.add(e.to(), e.weight() + e.weight());
+                if (distTo.get(p) + w < distTo.get(q)) {
 
+                    distTo.put(q, distTo.get(p) + w);
+                    edgeTo.put(q, p);
 
+                    if(fringe.contains(q)) {
+                        fringe.changePriority(q, distTo.get(q) + heuristic);
+                    } else {
+                        fringe.add(q, distTo.get(q) + heuristic);
+                    }
+                }
             }
         }
     }
+
     @Override
-    public SolverOutcome outcome() { return outcome; }
+    public SolverOutcome outcome() {
+        return outcome;
+    }
+
     @Override
-    public List<Vertex> solution(){ return solution; }
+    public List<Vertex> solution() {
+        return solution;
+    }
+
     @Override
-    public double solutionWeight(){ return solutionWeight; }
+    public double solutionWeight() {
+        return solutionWeight;
+    }
+
     @Override
-    public int numStatesExplored(){ return statesExplored; }
+    public int numStatesExplored() {
+        return statesExplored;
+    }
+
     @Override
-    public double explorationTime() { return exploreTime; }
+    public double explorationTime() {
+        return exploreTime;
+    }
+}
